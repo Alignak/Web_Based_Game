c9115edb6882ab901e7a78bda4b152e93a8157f9
Alignak <email_id>
2016-10-14 08:15:07 +0100

    Initial Push
    All work done up to now placed in the git, this includes: 
     A start to the top toolbar for logged in users
     Bootstrap files added

---
+++ assets/loggedHeader.php
@@ -0,0 +1,23 @@
+<!-- Bring in the navbar -->
+<nav class="navbar navbar-inverse navbar-static-top">
+
+    <!-- Set the navbar to spread across the entire width of the page -->
+    <div class="container-fluid">
+
+        <!-- This unordered list contains the guts of the navbar in the left -->
+        <ul class="nav navbar-nav">
+            <!-- The first link is to the homepage -->
+            <li><a href="index.php"><span class="glyphicon glyphicon-home"></span></a></li>
+            <!-- The next link is to the page the game is actually on -->
+            <li><a href="game.php">Play</a></li>
+            <!-- The last link is to the highscores page -->
+            <li><a href="highscores.php">Highscores</a></li>
+        </ul>
+
+        <!-- This unordered list contains the guts of the navbar in the right -->
+        <ul class="nav navbar-nav navbar-right">
+            <li><a>Welcome @NICK ENTER THE USERNAME HERE@</a></li>
+            <li><a href="index.php"><span class="glyphicon glyphicon-log-out"></span> Log Out</a></li>
+        </ul>
+    </div>
+</nav>
